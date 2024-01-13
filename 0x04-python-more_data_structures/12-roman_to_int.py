05e551e9b0e37fe9aa479ae720846194a4c5878f
KamandeRick <rickmwangi237@gmail.com>
2024-01-13 00:32:45 -0800

    add function toconvert roman numerals to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    romDict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+            }
+    if roman_string and isinstance(roman_string, str):
+        total = 0
+        keyLast = 0
+        keyCurrent = 0
+        for symbol in roman_string:
+            keyCurrent = romDict.get(symbol, 0)
+            if keyLast != 0 and keyLast >= keyCurrent:
+                total += keyCurrent
+            elif keyLast != 0 and keyLast < keyCurrent:
+                total += (keyCurrent - (2 * keyLast))
+            elif keyLast == 0:
+                total += keyCurrent
+            keyLast = keyCurrent
+        return total
+    else:
+        return 0
