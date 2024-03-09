c35ad541f4c578d0c64ddf5d436c0169ac64e86b
KamandeRick <rickmwangi237@gmail.com>
2024-03-09 07:25:20 -0800

    add class Square

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+ #   count = 0
+  #  while x > 0:
+   #     try:
+    #        print("{}".format(my_list[count]), end="")
+     #       count += 1
+      #      x -= 1
+       # except (IndexError, ValueError, TypeError):
+        #    pass
+        #except Exception:
+         #   print("Catch all")
+        #finally:
+         #   print("")
+#    return count
     count = 0
-    while x > 0:
+    for i in range(x):
         try:
-            print("{}".format(my_list[count]), end="")
+            print("{}".format(my_list[i]), end="")
+            #print("\n")
             count += 1
-            x -= 1
-        except (IndexError, ValueError, TypeError):
-            pass
-        except Exception:
-            print("Catch all")
-        finally:
-            print("")
+        except IndexError:
+            break
+        #finally:
+            #print("",end = "")
+    print("")
     return count
