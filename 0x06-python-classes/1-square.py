91bc855a45c68ea784afc7c1aacbdd73698406f9
KamandeRick <rickmwangi237@gmail.com>
2024-03-09 08:02:26 -0800

    add class square with private instance attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+Square Module to write a Square class
+which defines a square based on 0-square.py
+Author Fredrick Kamande
+'''
+
+
+class Square:
+    '''Definition of square class
+    Attributes:
+    __size
+
+    Methods:
+    __init__
+    '''
+    def __init__(self, size):
+        self.__size = size
+        '''
+
+        Private instance attribute: size
+        inatantiation with size
+        '''
