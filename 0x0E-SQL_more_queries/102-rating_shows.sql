275865417d092b8e1aa0c32ba009682a52f00756
Fred <rickmwangi237@gmail.com>
2024-03-28 08:32:36 -0700

    list all shows by rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -4,4 +4,16 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-
+SELECT 
+    tv_shows.title, 
+    SUM(tv_show_ratings.rate) AS rating 
+FROM 
+    tv_shows 
+INNER JOIN 
+    tv_show_ratings 
+ON 
+    tv_show_ratings.show_id = tv_shows.id 
+GROUP BY 
+    tv_shows.title 
+ORDER BY 
+    rating DESC;
