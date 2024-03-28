63c7fb03ae7d2ecfe8a29d248c7cfcc548480dcd
Fred <rickmwangi237@gmail.com>
2024-03-28 08:31:06 -0700

    put in correct code

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -5,32 +5,32 @@
 -- You can use a maximum of two SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT
-    tv_genres.name
-FROM
-    tv_genres
-LEFT JOIN (
-    SELECT
-        tv_genres.id,
-        tv_genres.name
-    FROM
-        tv_genres
-    INNER JOIN
-        tv_show_genres
-    ON
-        tv_genres.id = tv_show_genres.genre_id
-    INNER JOIN
-        tv_shows
-    ON
-        tv_show_genres.show_id = tv_shows.id
-    WHERE
-        tv_shows.title = "Dexter"
-    ORDER BY
-        tv_genres.id
-) AS dexter_genres
-ON
-    dexter_genres.id = tv_genres.id
-WHERE
-    dexter_genres.id IS NULL
-ORDER BY
-    tv_genres.name;
+SELECT 
+    tv_shows.title 
+FROM 
+    tv_shows 
+LEFT JOIN 
+    (
+    SELECT 
+        tv_shows.title 
+    FROM 
+        tv_shows 
+    INNER JOIN 
+        tv_show_genres 
+    ON 
+        tv_show_genres.show_id = tv_shows.id 
+    INNER JOIN 
+        tv_genres 
+    ON 
+        tv_genres.id = tv_show_genres.genre_id 
+    WHERE 
+        tv_genres.name = "Comedy" 
+    ORDER BY 
+        tv_shows.id
+    ) AS comedy_shows 
+ON 
+    comedy_shows.title = tv_shows.title 
+WHERE 
+    comedy_shows.title IS NULL 
+ORDER BY 
+    tv_shows.title;
