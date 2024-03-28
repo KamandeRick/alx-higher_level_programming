b2a17a104c0e771aa93b984a90a50c1106ca8053
Fred <rickmwangi237@gmail.com>
2024-03-28 07:44:18 -0700

    syntax error handled

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,8 +7,8 @@
 -- If the table cities already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_us.cities(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	state_id INT,
+	id INT UNIQUE  AUTO_INCREMENT NOT NULL PRIMARY KEY
+	state_id INT NOT NULL,
 	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
 	name VARCHAR(256) NOT NULL
 	);
