9f52a4c9128b2084d1b1f366d922fb451beed7f5
kamanderick <124000425+KamandeRick@users.noreply.github.com>
2024-07-24 14:08:20 +0300

    add if loop to check type of arguments

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
+
 """
-Module defining a function that takes 2 arguments and returns the sum of
-the 2 arguments
+Module defining a function that takes 2 arguments
+returns the sum of the 2 arguments
 """
+
+
 def add_integer(a, b=98):
     """
-        function that takes 2 arguments of type int and returns their sum
+        function that takes 2 arguments of type int or float
+        returns their sum
     """
-    if ((type(a) != int) and (type(a) != float)) or ((type(b) != int) and (type(b) != float)):
-        raise TypeError 
-    elif type(a) == float or type(b) == float:
+
+    if ((type(a) != int) and (type(a) != float)):
+        raise TypeError("a must be an integer")
+    if ((type(b) != int) and (type(b) != float)):
+        raise TypeError("b must be an integer")
+    
+    if type(a) == float or type(b) == float:
         a = int(a)
         b = int(b)
     
-    result = a+b
+    result = a + b
     return result
