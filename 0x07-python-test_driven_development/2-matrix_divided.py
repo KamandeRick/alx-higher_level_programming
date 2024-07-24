1cd8ecb1da1ba04eee19e71cb856aaa65e61e4f4
kamanderick <124000425+KamandeRick@users.noreply.github.com>
2024-07-24 21:18:19 +0300

    add check for matrix element length and round method

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -18,18 +18,18 @@ def matrix_divided(matrix, div):
             raise TypeError(
                 "matrix must be a matrix (list of lists) of integers/floats"
                 )
-        if not all(len(row) == len(matrix[0])):
-            raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats"
-                )
         for column in row:
             if type(column) != int and type(column) != float:
                 raise TypeError(
                     """matrix must be a matrix (list of lists)
                     of integers/floats"""
                     )
+    if not all(len(row) == len(matrix[0])for row in matrix):
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+                )
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return [[column / div for column in row] for row in matrix]
+    return [[round(column / div, 2) for column in row] for row in matrix]
