bc6cf1a57ae51f4e8bf3d5b3715d6df5c68c3901
Fred <rickmwangi237@gmail.com>
2024-07-25 01:43:09 -0700

    edit f string to make it non empty by passing full_name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -21,4 +21,4 @@ def say_my_name(first_name, last_name=""):
     if type(last_name) != str:
         raise TypeError('last_name must be a string')
     full_name = (first_name + " " + last_name).title()
-    return full_name
+    print(f"My name is {full_name}")
