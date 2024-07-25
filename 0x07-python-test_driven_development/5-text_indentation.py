294ec5bbc144120205182b03b75637840fc44337
kamanderick <124000425+KamandeRick@users.noreply.github.com>
2024-07-25 22:13:57 +0300

    add module docstrings for

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""
+Module defining a text indentation function
+The function takes in a string argument
+Inserts 2 newline characters after '?', '.', or ':'
+Prints new result text
+"""
+
