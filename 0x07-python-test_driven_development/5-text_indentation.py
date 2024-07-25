010b60e98b760fc0f93756e175d0eaf04d7a835b
kamanderick <124000425+KamandeRick@users.noreply.github.com>
2024-07-25 22:16:56 +0300

    add func prototype and docs

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,3 +7,13 @@ Inserts 2 newline characters after '?', '.', or ':'
 Prints new result text
 """
 
+
+def text_indentation(text):
+    """
+    The function takes in a string argument
+    Inserts 2 newline characters after '?', '.', or ':'
+    Prints new result text
+    """
+
+    if type(text) != str:
+        raise TypeError("text must be a string")
